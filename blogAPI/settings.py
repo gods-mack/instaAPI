66158b7f1cd491457ed443c858188ef99dc54abe
gods-mack <manish-1004@outlook.com>
2021-03-25 02:24:19 +0530

    static and media root

--- blogAPI/settings.py
+++ blogAPI/settings.py
@@ -141,8 +141,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/profile_pics/'
+
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
